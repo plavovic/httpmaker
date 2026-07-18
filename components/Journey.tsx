8eaeff0f2b6d23e2cd8cfe9d640624a15ac24e54
plavovic <220302234@student.ius.edu.ba>
2026-07-18 14:10:52 +0200

    login pipeline adjusted

--- components/Journey.tsx
+++ components/Journey.tsx
@@ -262,7 +262,7 @@ export function Journey() {
 
     <div className="mt-10 flex flex-col items-center justify-center gap-5">
 
-    <Link href="/editor">
+    <Link href="/login">
       <Button />
     </Link>
         
